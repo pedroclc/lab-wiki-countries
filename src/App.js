cf6609516c36537783222dda7f16f00cf20e01ed
Pedro Costa <pedroclc@gmail.com>
2022-11-14 08:35:16 -0300

    WDFT Lab w4d5 14-11-2022

--- src/App.js
+++ src/App.js
@@ -3,11 +3,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar/Navbar';
 import CountriesList from './components/CountriesList/CountriesList';
 import CountryDetails from './components/CountryDetails/CountryDetails';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        {/* <Route path="" element={} />
+        <Route path="" element={} />
+        <Route path="" element={} /> */}
+      </Routes>
       <Navbar />
+      <CountriesList />
     </div>
   );
 }
--- src/components/CountriesList/CountriesList.js
+++ src/components/CountriesList/CountriesList.js
@@ -1,1 +1,24 @@
+import countries from '../../countries.json';
 
+export default function CountriesList() {
+  //console.log(countries);
+
+  const countriesList = countries.map((country, index) => {
+    // se não usar key o programa dá vários warnings
+    // key é um identificador para tornar cada elemento único
+    return (
+      <div className="col-5" key={index}>
+        <div className="list-group">
+          {/* <img src="https://flagpedia.net/data/flags/icon/72x54/br.png" /> */}
+          <a
+            className="list-group-item list-group-item-action"
+            href={country.alpha3Code}
+          >
+            {country.alpha2Code} {country.name.official}
+          </a>
+        </div>
+      </div>
+    );
+  });
+  return <div>{countriesList}</div>;
+}
